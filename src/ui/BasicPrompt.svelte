b9311cb45aba24da26accc7047e206ebe4b41bd3
reidpat <61036489+reidpat@users.noreply.github.com>
2023-06-10 15:24:12 -0700

    Successfully making fetch requests to openAI chat (like a pirate, arrrr!)

--- src/ui/BasicPrompt.svelte
+++ src/ui/BasicPrompt.svelte
@@ -1,28 +1,51 @@
 <script lang="ts">
-    import 'dotenv/config'
-    import { OpenAI } from "langchain/llms/openai";
-    
+    import "dotenv/config";
+    import { requestUrl } from "obsidian";
 
-    let promptInput = ""
-    let promptOutput = "res..."
-    export const  openAIKey: string = "";
-
-    const model = new OpenAI({ openAIApiKey: openAIKey, temperature: 0.4 });
-
+    let systemInput = "talk like a pirate"
+    let promptInput = "congratulate me on completing this code test";
+    let promptOutput = "res...";
+    export let openAIKey: string = "";
 
     async function sendPrompt(promptInput: string): Promise<void> {
         console.log("input: " + promptInput);
         console.log("api-key: " + openAIKey);
-        // const response = await model.call(promptInput);
-        // promptOutput = response;
+        const params = {
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: promptInput }],
+            max_tokens: "100",
+        };
+        const response = await requestUrl({
+            url: `https://api.openai.com/v1/chat/completions`,
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{role: "system", content: systemInput},{ role: "user", content: promptInput }],
+                temperature: 0.2,
+                max_tokens: 100,
+            }),
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                Authorization:
+                    "Bearer " +
+                    "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",
+            },
+        });
+        console.log(response);
+        promptOutput = response.json.choices[0].message.content;
     }
-
 </script>
-
 
 <div>
     <h1>Basic Prompt</h1>
-    <input type="text" bind:value={promptInput}>
-    <button on:click={() => {sendPrompt(promptInput)}}>Send Prompt</button>
+    <label for="system">System Input</label>
+    <input name="system" type="text" bind:value={systemInput} />
+    <label for="user">User</label>
+    <input name="user" type="text" bind:value={promptInput} />
+    <button
+        on:click={() => {
+            sendPrompt(promptInput);
+        }}>Send Prompt</button
+    >
     <span>{promptOutput}</span>
 </div>
